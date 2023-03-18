89d61b3883f01ac52c6c1e754aedc7809c93d242
Sebastian Rowan <79868066+sebastianrowan@users.noreply.github.com>
2023-03-17 21:32:08 -0400

    document

--- R/svibuildr-package.R
+++ R/svibuildr-package.R
@@ -1,21 +1,27 @@
 #' Return a social vulnerability index for a specified region according to the
-#' CDC ADSTR methodology using the tidycensus package developed by Kyle Walker.
+#' CDC ATSDR methodology using the tidycensus package developed by Kyle Walker.
 #'
 #' This packages uses US Census Bureau data but is neither endorsed nor
 #' supported by the US Census Bureau.
 #'
 #' This package used the SVI methodology published by the CDC Agency for
-#' Toxic Substances and Disease Registry (ADSTR) but is neither endorsed nor
+#' Toxic Substances and Disease Registry (ATSDR) but is neither endorsed nor
 #' supported by the ADSTR.
 #'
 #' @author Sebastian Rowan
-#' @name svibuildr
+#' @name sviBuildr
 #' @docType package
-#' @import sf
+#' @import cli
+#' @import withr
+#' @import magrittr
+#' @importFrom magrittr %>%
 #' @import dplyr
-#' @import tigris
 #' @import stringr
+#' @importFrom glue glue
+#' @importFrom lifecycle deprecated
 #' @importFrom rlang inform abort arg_match sym
 #' @import tidyselect
+#' @import sf
+#' @import tigris
 #' @import tidycensus
 NULL
