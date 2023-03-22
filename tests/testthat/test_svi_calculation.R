4095a776aac17f14287842f3745244780c0ce331
Sebastian Rowan <79868066+sebastianrowan@users.noreply.github.com>
2023-03-22 11:19:59 -0400

    combine global var files

---
+++ tests/testthat/test_svi_calculation.R
@@ -0,0 +1,35 @@
+test_that(
+    "Properly downloads and calculates single state 2020 SVI",
+    {
+        x <- calculate_svi("county", year = 2020, state = "nh")
+        expect_s3_class(x, "data.frame")
+    }
+)
+
+test_that(
+    "Properly downloads and calculates multi-state 2020 SVI",
+    {
+        x <- calculate_svi("county", year = 2020, state = c(33, "Vermont", "Me"))
+        expect_s3_class(x, "data.frame")
+    }
+)
+
+test_that(
+  "Refuses to download and calculate US SVI",
+  {
+    expect_error(
+      calculate_svi("county", year = 2020, state = "us"),
+      "Calculating US svi not currently supported"
+    )
+  }
+)
+
+test_that(
+  "Refuses to calculate SVI for year != 2020",
+  {
+    expect_error(
+      calculate_svi("county", year = 2018, state = "nh"),
+      "SVI calculation currently only available for year 2020."
+    )
+  }
+)
