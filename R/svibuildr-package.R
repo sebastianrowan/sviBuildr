9c9f77542a7696be839fadcb720d86e2a28b61e0
Sebastian Rowan <79868066+sebastianrowan@users.noreply.github.com>
2023-05-04 15:47:52 -0400

    namespace

--- R/svibuildr-package.R
+++ R/svibuildr-package.R
@@ -13,8 +13,6 @@
 #' @docType package
 #' @import cli
 #' @import dplyr
-#' @import tidyr
-#' @import magrittr
 #' @import sf
 #' @import stringr
 #' @import tidycensus
@@ -22,7 +20,7 @@
 #' @import tigris
 #' @import withr
 #' @import readr
-#' @importFrom dplyr ntile
+#' @importFrom tidyr replace_na
 #' @importFrom glue glue
 #' @importFrom lifecycle deprecated
 #' @importFrom magrittr %>%
